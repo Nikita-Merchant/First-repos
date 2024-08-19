03283374590635cc4c9d31bb211624a1d8db1641
Nikita-Merchant <mashykkov@mail.ru>
2024-08-20 00:17:44 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = 'Nikita'
+print(f'Name: {name}')
+age = 33
+print(f'Age: {age}')
+age = age + 18
+print(f'New Age: {age}')
+is_student = True
+print(f'Is Student: {is_student}')
+#прикрепил архивный файл, поскольку для файлов с расширением .py платформа пишет - "Запрещенный тип файла!"
