54dd67805e4a1427cfc63d3e5dcf132be3d320a3
Nikita-Merchant <mashykkov@mail.ru>
2024-09-13 02:00:26 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(first=1, second=0):
+    if second != 0:
+        return first / second
+    else:
+        return 'Ошибка'
---
+++ true_math.py
@@ -0,0 +1,6 @@
+def divide(first=1, second=0):
+    from math import inf
+    if second != 0:
+        return first / second
+    else:
+        return inf
