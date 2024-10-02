00a3374a3e41f70ae10a8bcb5710887166718f4e
Nikita-Merchant <mashykkov@mail.ru>
2024-10-03 00:47:19 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,27 @@
+class House:
+    houses_history = []
+    def __new__(cls, name: str, number_of_floors: int):
+        cls.houses_history.append(name)
+        return object.__new__(cls)
+
+    def __init__(self, name: str, number_of_floors: int):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+del h2
+del h3
+
+print(House.houses_history)
