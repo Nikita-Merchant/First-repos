10f28d0b658ad919d95a712621779a3ca728b9d4
Nikita-Merchant <mashykkov@mail.ru>
2024-10-20 00:54:30 +0300

    Add files via upload

---
+++ module_8_3.py
@@ -0,0 +1,61 @@
+class IncorrectVinNumber(Exception):
+    def __init__(self, message):
+        self.message = message
+
+class IncorrectCarNumbers(IncorrectVinNumber):
+    pass
+
+class Car:
+    def __init__(self, model: str, vin: int, numbers: str):
+        self.model = model
+        self.__vin = vin
+        self.__numbers = numbers
+        self.__is_valid_vin(self.__vin)
+        self.__is_valid_numbers(self.__numbers)
+    def __is_valid_vin(self, vin_number):
+        if isinstance(self.__vin, int):
+            if self.__vin in range(1000000, 9999999 + 1):
+                return True
+            else:
+                raise IncorrectVinNumber('Неверный диапазон для vin номера')
+        else:
+            raise IncorrectVinNumber('Некорректный тип vin номер')
+    def __is_valid_numbers(self, numbers):
+        if isinstance(self.__numbers, str):
+            if len(self.__numbers) == 6:
+                return True
+            else:
+                raise IncorrectCarNumbers('Неверная длина номера')
+        else:
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+
+if __name__ == '__main__':
+    try:
+        first = Car('Model1', 1000000, 'f123dj')
+    except IncorrectVinNumber as exc:
+        print(exc.message)
+    except IncorrectCarNumbers as exc:
+        print(exc.message)
+    else:
+        print(f'{first.model} успешно создан')
+
+    try:
+        second = Car('Model2', 300, 'т001тр')
+    except IncorrectVinNumber as exc:
+        print(exc.message)
+    except IncorrectCarNumbers as exc:
+        print(exc.message)
+    else:
+        print(f'{second.model} успешно создан')
+
+    try:
+        third = Car('Model3', 2020202, 'нет номера')
+    except IncorrectVinNumber as exc:
+        print(exc.message)
+    except IncorrectCarNumbers as exc:
+        print(exc.message)
+    else:
+        print(f'{third.model} успешно создан')
+
+
+
