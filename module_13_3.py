a40808864a7b2e29f7c80376f08e6eb42227a1f7
Nikita-Merchant <mashykkov@mail.ru>
2024-12-05 23:48:28 +0300

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,18 @@
+import asyncio
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+api = '7923144183:AAHNGZPLYb1HXesLKxyUgluY79le8wDC6l0'
+bot = Bot(token=api)
+disp = Dispatcher(bot, storage=MemoryStorage())
+
+@disp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+@disp.message_handler()
+async def all_massages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(disp, skip_updates=True)
