2e1649920f2ea77a9b90cb36c71080c985e3fc80
Nikita-Merchant <mashykkov@mail.ru>
2025-01-21 15:38:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-#1st program
-print(9 ** 0.5 * 5)
-#2nd program
-print(9.99 > 9.98 and 1000 != 1000.1)
-#3rd program
-print(2 * 2 + 2)
-print(2 * (2 + 2))
-print((2 * 2 + 2) == (2 * (2 + 2)))
-#4th program
-#print('123.456'[1 + '123.456'.find('.')])
-print(int(float('123.456') * 10) % 10)
+from fastapi import FastAPI
+
+# Создаем экземпляр приложения FastAPI
+app = FastAPI()
+
+# Определение базового маршрута
+@app.get("/")
+async def main_page():
+    return "Главная страница"
+
+@app.get("/user/admin")
+async def admin_page():
+    return "Вы вошли как администратор"
+
+# Определение базового маршрута
+@app.get("/user/{user_id}")
+async def user_id_page(user_id: int = 1):
+    return f"Вы вошли как пользователь № {user_id}"
+
+# Определение базового маршрута
+@app.get("/user")
+async def user_id_page(username: str = 'Nick', age: int = 34):
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
